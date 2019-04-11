3508b71ae3bb1cc0264f0d641b29137ce975741d
Justin_Jung <jungdw0624@gmail.com>
2019-04-11 14:48:17 +0900

    clock

---
+++ clock.js
@@ -0,0 +1,24 @@
+const clockContainer = document.querySelector(".js-clock");
+const clockTitle = clockContainer.querySelector("h1");
+
+
+function getTime(){
+    const date = new Date();
+    const hour = date.getHours();
+    const min = date.getMinutes();
+    const sec = date.getSeconds();
+    
+    clockTitle.innerText = `${
+        hour<10 ? `0${hour}`:hour
+    }:${
+        min<10 ? `0${min}`:min
+    }:${
+        sec<10 ? `0${sec}`:sec
+     }`;
+}
+
+function init(){
+    setInterval(getTime, 100);
+}
+
+init();
